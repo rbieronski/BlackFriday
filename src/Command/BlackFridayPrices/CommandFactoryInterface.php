afcf202b13f88e4bea641f23e38bb47a3ffae17b
Rafał Bieroński <bieronski@yahoo.pl>
2021-01-20 16:32:00 +0100

    removed unnecessary use statement

--- src/Command/BlackFridayPrices/CommandFactoryInterface.php
+++ src/Command/BlackFridayPrices/CommandFactoryInterface.php
@@ -2,8 +2,6 @@
 
 namespace Anguis\BlackFriday\Command\BlackFridayPrices;
 
-use Anguis\BlackFriday\Command\CommandInterface;
-
 interface CommandFactoryInterface
 {
     public function create(bool $sort): ShowPricesCommand;
